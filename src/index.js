b1c7301c850dd3d302379434a36334b250ba91a5
Agustin Carnevale <agustincarnevale@Agustins-MacBook-Pro.local>
2020-07-09 12:19:44 -0300

    set up engines in package.json for deployment in heroku

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'
 import store from './store'
 import App from './components/App';
-import 'materialize-css/dist/css/materialize.min.css';
 
 ReactDOM.render(
   <Provider store={store}>
